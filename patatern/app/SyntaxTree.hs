417fe1129ed6ff6827bad9631b8cd8b73fd44dad
Francesco Magliocca <franciman12@gmail.com>
2021-12-06 16:51:03 +0100

    Define Syntax tree

---
+++ patatern/app/SyntaxTree.hs
@@ -0,0 +1,15 @@
+data Term v
+  = Symbol Text
+  | Int Integer
+  | Var v
+  | (:<) (Term v) (Term v)
+  | Lazy (Term v)
+  deriving (Eq, Show, Data)
+ 
+infixr 5 :<
+ 
+data Rule v = Rule
+  { lhs :: Term v,
+    rhs :: Term v
+  }
+  deriving (Eq, Show)
