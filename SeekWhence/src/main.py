eede86e429b2e73ba45a69ed7a1780c9facc8d87
Katrina Scialdone <kat@swordglowsblue.com>
2021-12-07 02:46:28 -0700

    Dec 6: 2h, slice arithmetic, change custom base index to slice sugar, sequence equality and constant folding

--- SeekWhence/src/main.py
+++ SeekWhence/src/main.py
@@ -16,41 +16,38 @@ from abc import ABC, abstractmethod
 class SequenceLike(ABC):
   @abstractmethod
   def generator(self, ctx={}):
-    pass
+    '''Generator function which yields each step of the sequence from 0.'''
 
   @abstractmethod
   def value_at(self, n, ctx={}, prev=None):
-    pass
+    '''Returns the value of the step at index n, calculating the previous step if needed and prev is not passed.'''
 
 @dataclass
 class Sequence(SequenceLike):
-  exprs: tuple
   ident: str = "<anonymous>"
-  baseCase: list = None
-  baseIndx: int = 0
+  exprs: list = None
+  baseCases: list = None
 
   __cache = {}
 
   def __post_init__(self):
-    if self.baseIndx is None: self.baseIndx = 0
-    if self.baseIndx < 0 and abs(self.baseIndx) > len(self.baseCase):
-      raise Exception(f'Cannot define sequence {self.ident} with negative indices not covered by its base cases')
+    self.baseCases = [simplify_expr(e) for e in self.baseCases or []]
+    self.exprs = [simplify_expr(e) for e in self.exprs or []]
 
   def generator(self, ctx={}):
-    current, prev = max(self.baseIndx or 0, 0), None
+    '''Generator function which yields each step of the sequence from 0.'''
+    current, prev = 0, None
     while True: 
       yield (nextval := self.value_at(current, ctx, prev))
       current, prev = current+1, nextval
 
   def value_at(self, n, ctx={}, prev=None):
-    if n < self.baseIndx: return 0
+    '''Returns the value of the step at index n, calculating the previous step if needed and prev is not passed.'''
+    if n < 0: return 0
     if n in self.__cache: return self.__cache[n]
 
-    if self.baseCase is not None and n < len(self.baseCase)+self.baseIndx:
-      if self.baseIndx < 0:
-        self.__cache[n] = exec_expr(self.baseCase[n-self.baseIndx], ctx)
-      else:
-        self.__cache[n] = exec_expr(self.baseCase[n], ctx)
+    if self.baseCases is not None and n < len(self.baseCases):
+      self.__cache[n] = exec_expr(self.baseCases[n], ctx)
     else:
       if prev is None and find_in_expr(self.exprs[n % len(self.exprs)], 'x', 'S'): prev = self.value_at(n-1, ctx)
       self.__cache[n] = exec_expr(self.exprs[n % len(self.exprs)], { **ctx, 'x': prev, 'n': n, 'S': self })
@@ -59,54 +56,101 @@ class Sequence(SequenceLike):
 
   def __str__(self):
     exprs = ", ".join([display_expr(e) for e in self.exprs])
-    if self.baseCase is not None:
-      basec = ", ".join([display_expr(i) for i in self.baseCase])
+    if len(self.baseCases) > 0:
+      basec = ", ".join([display_expr(i) for i in self.baseCases])
       return f"[sequence {self.ident}: {basec} | {exprs}]"
     return f"[sequence {self.ident}: {exprs}]"
 
-  def wrap_op(self, op, opr, flip=False, mode='both'):
+  @staticmethod
+  def build(ident, exprs, baseCases=None, sliceIndx=None):
+    '''Factory function; if passed a slice index, returns a slice of the created sequence instead.'''
+    sequence = Sequence(ident, exprs, baseCases)
+    return sequence if sliceIndx is None else SequenceSlice(sequence, sliceIndx)
+
+  # Operator overloading for arithmetic operators to wrap base cases / expression
+  @staticmethod
+  def build_op(op, mode):
+    def _sequence_op(ctx, x, y):
+      if isinstance(x, Sequence): return x.__wrap_op(op, y, flip=False, mode=mode)
+      if isinstance(y, Sequence): return y.__wrap_op(op, x, flip=True,  mode=mode)
+      if isinstance(x, SequenceSlice): return SequenceSlice(x.__wrap_op(op, y, flip=False, mode=mode), x.sliceIndx)
+      if isinstance(y, SequenceSlice): return SequenceSlice(y.__wrap_op(op, x, flip=True,  mode=mode), y.sliceIndx)
+      return operators[op](x,y)
+    return _sequence_op 
+
+  def __wrap_op(self, op, opr, flip=False, mode='both'):
     if isinstance(opr, SequenceLike):
       raise Exception(f'Arithmetic operations on multiple sequences are not currently supported')
     else:
       new_exprs = [(op, opr if flip else e, e if flip else opr) for e in self.exprs]
-      new_basec = [(op, opr if flip else e, e if flip else opr) for e in self.baseCase] if self.baseCase is not None else None
+      new_basec = [(op, opr if flip else e, e if flip else opr) for e in self.baseCases] if self.baseCases is not None else None
       op        = op + ('~' if mode == 'base' else ':' if mode == 'expr' else '')
 
       return copy_dc(self,
         ident = f'<{display_expr(opr)}{op}{self.ident}>' if flip else f'<{self.ident}{op}{display_expr(opr)}>',
         exprs = new_exprs if mode != 'base' else self.exprs,
-        baseCase = new_basec if mode != 'expr' else self.baseCase)
+        baseCases = new_basec if mode != 'expr' else self.baseCases)
 
-  def __add__(self, other): return self.wrap_op('+', other)
-  def __sub__(self, other): return self.wrap_op('-', other)
-  def __mul__(self, other): return self.wrap_op('*', other)
-  def __truediv__(self, other): return self.wrap_op('/', other)
-  def __mod__(self, other): return self.wrap_op('%', other)
-  def __pow__(self, other): return self.wrap_op('^', other)
+  __add__     = lambda s,o: s.__wrap_op('+', o); __radd__     = lambda s,o: s.__wrap_op('+', o, True)
+  __sub__     = lambda s,o: s.__wrap_op('-', o); __rsub__     = lambda s,o: s.__wrap_op('-', o, True)
+  __mul__     = lambda s,o: s.__wrap_op('*', o); __rmul__     = lambda s,o: s.__wrap_op('*', o, True)
+  __truediv__ = lambda s,o: s.__wrap_op('/', o); __rtruediv__ = lambda s,o: s.__wrap_op('/', o, True)
+  __mod__     = lambda s,o: s.__wrap_op('%', o); __rmod__     = lambda s,o: s.__wrap_op('%', o, True)
+  __pow__     = lambda s,o: s.__wrap_op('^', o); __rpow__     = lambda s,o: s.__wrap_op('^', o, True)
 
-  def __radd__(self, other): return self.wrap_op('+', other, True)
-  def __rsub__(self, other): return self.wrap_op('-', other, True)
-  def __rmul__(self, other): return self.wrap_op('*', other, True)
-  def __rtruediv__(self, other): return self.wrap_op('/', other, True)
-  def __rmod__(self, other): return self.wrap_op('%', other, True)
-  def __rpow__(self, other): return self.wrap_op('^', other, True)
+  def __eq__(self, other):
+    if not isinstance(other, SequenceLike): return False
+    basec = all(expr_equal(a,b) for a,b in zip(self.baseCases or [], other.baseCases or []))
+    exprs = all(expr_equal(a,b) for a,b in zip(self.exprs or [], other.exprs or []))
+    slidx = not isinstance(other, SequenceSlice) or other.sliceIndx == 0
+    return basec and exprs and slidx
 
 @dataclass
 class SequenceSlice(SequenceLike):
-  sequence: Sequence
+  sequence: SequenceLike
   sliceIndx: int
 
+  @property 
+  def ident(self): return self.sequence.ident
+  @property
+  def exprs(self): return self.sequence.exprs
+  @property 
+  def baseCases(self): return self.sequence.baseCases
+
+  def __post_init__(self):
+    if isinstance(self.sequence, SequenceSlice):
+      self.sliceIndx = self.sliceIndx + self.sequence.sliceIndx
+      self.sequence  = self.sequence.sequence
+
   def generator(self, ctx={}):
-    current, prev = max(self.sliceIndx, 0), 0
+    '''Generator function which yields each step of the sequence from 0.'''
+    current, prev = max(self.sliceIndx, 0), None
     while True: 
       yield (nextval := self.sequence.value_at(current, ctx, prev))
       current, prev = current+1, nextval
 
   def value_at(self, n, ctx={}, prev=None):
+    '''Returns the value of the step at index n, calculating the previous step if needed and prev is not passed.'''
     return self.sequence.value_at(n+self.sliceIndx, ctx)
 
   def __str__(self):
-    return f"[slice {self.sequence.ident}::{self.sliceIndx}]"
+    return f"[slice {self.ident}::{self.sliceIndx}]"
+
+  # Pass arithmetic operators through to the underlying sequence, wrapping the result in a new slice
+  def __pass_op(get_op): 
+    return lambda self,other: SequenceSlice(get_op(self.sequence)(other), self.sliceIndx)
+
+  __add__     = __pass_op(lambda s: s.__add__);     __radd__     = __pass_op(lambda s: s.__radd__)
+  __sub__     = __pass_op(lambda s: s.__sub__);     __rsub__     = __pass_op(lambda s: s.__rsub__)
+  __mul__     = __pass_op(lambda s: s.__mul__);     __rmul__     = __pass_op(lambda s: s.__rmul__)
+  __truediv__ = __pass_op(lambda s: s.__truediv__); __rtruediv__ = __pass_op(lambda s: s.__rtruediv__)
+  __mod__     = __pass_op(lambda s: s.__mod__);     __rmod__     = __pass_op(lambda s: s.__rmod__)
+  __pow__     = __pass_op(lambda s: s.__pow__);     __rpow__     = __pass_op(lambda s: s.__rpow__)
+
+  def __eq__(self, other):
+    if not isinstance(other, SequenceLike): return False
+    if isinstance(other, Sequence): return self.sliceIndx == 0 and self.sequence == other
+    return self.sliceIndx == other.sliceIndx and self.sequence == other.sequence
 
 Identifier   = namedtuple('ident',   ['name'])
 LoopStmt     = namedtuple('loop',    ['amount', 'srcseq', 'ident', 'body'])
@@ -181,22 +225,14 @@ def create_parser():
   statements = newline.optional() >> padding >> statement.sep_by(newline, min=1) << padding << newline.optional()
 
   # Sequence definitions
-  sequenceDef = alt(
-    seq(
-      _1    = string('sequence') << whitespace,
-      ident = validident << whitespace,
-      _2    = string('=') << whitespace,
-      exprs = expression.sep_by(padding >> string(',') << padding, min=1)
-    ).combine_dict(Sequence),
-    seq(
-      _1       = string('sequence') << whitespace,
-      ident    = validident << whitespace,
-      baseCase = string('from') >> whitespace >> operand.sep_by(padding >> string(',') << padding, min=1) << whitespace,
-      baseIndx = (string('at') >> whitespace >> integer << whitespace).optional(),
-      _2       = string('=') << whitespace,
-      exprs    = expression.sep_by(padding >> string(',') << padding, min=1)
-    ).combine_dict(Sequence)
-  )
+  sequenceDef = seq(
+    _1        = string('sequence') << whitespace,
+    ident     = validident,
+    sliceIndx = (string('::') >> integer).optional(),
+    baseCases = (whitespace >> string('from') >> whitespace >> operand.sep_by(padding >> string(',') << padding, min=1)).optional(),
+    _2        = whitespace >> string('=') << whitespace,
+    exprs     = expression.sep_by(padding >> string(',') << padding, min=1)
+  ).combine_dict(Sequence.build)
 
   # Function definitions and calls
   funcDef = seq(
@@ -260,15 +296,20 @@ default_ctx = {
   'print': lambda _,*s: print(*s),
   'input': lambda _,p=None: (input() if p is None else input(p)) if sys.stdin.isatty() else sys.stdin.readline().rstrip('\n'),
   'systime': lambda _: math.floor(systime()*1000),
+
   'floor': lambda _,x: math.floor(x),
   'ceil': lambda _,x: math.ceil(x),
   'round': lambda _,x: math.round(x),
+
+  'unslice': lambda _,x: x.sequence if isinstance(x,SequenceSlice) else x if isinstance(x,Sequence) else None,
+
   'true': True,
   'false': False,
   'none': None
 }
 
 operators = {
+  # Arithmetic
   '+': lambda _,x,y: x+y,
   '-': lambda _,x,y: x-y,
   '*': lambda _,x,y: x*y,
@@ -276,20 +317,15 @@ operators = {
   '%': lambda _,x,y: x%y if y != 0 else None,
   '^': lambda _,x,y: x**y,
 
-  '+~': lambda _,x,y: x.wrap_op('+', y, flip=isinstance(y,Sequence), mode="base") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['+'](x,y),
-  '-~': lambda _,x,y: x.wrap_op('-', y, flip=isinstance(y,Sequence), mode="base") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['-'](x,y),
-  '*~': lambda _,x,y: x.wrap_op('*', y, flip=isinstance(y,Sequence), mode="base") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['*'](x,y),
-  '/~': lambda _,x,y: x.wrap_op('/', y, flip=isinstance(y,Sequence), mode="base") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['/'](x,y),
-  '%~': lambda _,x,y: x.wrap_op('%', y, flip=isinstance(y,Sequence), mode="base") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['%'](x,y),
-  '^~': lambda _,x,y: x.wrap_op('^', y, flip=isinstance(y,Sequence), mode="base") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['^'](x,y),
+  # Sequence-specialized arithmetic
+  '+~': Sequence.build_op('+', "base"), '+:': Sequence.build_op('+', "expr"),
+  '-~': Sequence.build_op('-', "base"), '-:': Sequence.build_op('-', "expr"),
+  '*~': Sequence.build_op('*', "base"), '*:': Sequence.build_op('*', "expr"),
+  '/~': Sequence.build_op('/', "base"), '/:': Sequence.build_op('/', "expr"),
+  '%~': Sequence.build_op('%', "base"), '%:': Sequence.build_op('%', "expr"),
+  '^~': Sequence.build_op('^', "base"), '^:': Sequence.build_op('^', "expr"),
 
-  '+:': lambda _,x,y: x.wrap_op('+', y, flip=isinstance(y,Sequence), mode="expr") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['+'](x,y),
-  '-:': lambda _,x,y: x.wrap_op('-', y, flip=isinstance(y,Sequence), mode="expr") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['-'](x,y),
-  '*:': lambda _,x,y: x.wrap_op('*', y, flip=isinstance(y,Sequence), mode="expr") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['*'](x,y),
-  '/:': lambda _,x,y: x.wrap_op('/', y, flip=isinstance(y,Sequence), mode="expr") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['/'](x,y),
-  '%:': lambda _,x,y: x.wrap_op('%', y, flip=isinstance(y,Sequence), mode="expr") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['%'](x,y),
-  '^:': lambda _,x,y: x.wrap_op('^', y, flip=isinstance(y,Sequence), mode="expr") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['^'](x,y),
-
+  # Comparison
   '<':  lambda _,x,y: x<y,
   '<=': lambda _,x,y: x<=y,
   '>':  lambda _,x,y: x>y,
@@ -297,12 +333,13 @@ operators = {
   '==': lambda _,x,y: x==y,
   '!=': lambda _,x,y: x!=y,
 
-  ':': lambda c,x,y: x.value_at(math.floor(y),c) if isinstance(x,SequenceLike) and (isinstance(y,int) or isinstance(y,float)) else None,
-  '::': lambda _,x,y: SequenceSlice(x,y) if isinstance(x,SequenceLike) and (isinstance(y,int) or isinstance(y,float)) else None
+  # Sequence access / slicing
+  ':': lambda c,x,y: x.value_at(math.floor(y),c) if isinstance(x,SequenceLike) and isnumber(y) else None,
+  '::': lambda _,x,y: SequenceSlice(x,math.floor(y)) if isinstance(x,SequenceLike) and isnumber(y) else None
 }
 
 def exec_expr(expr, ctx={}):
-  '''Execute a parsed expression'''
+  '''Execute a parsed expression.'''
 
   if isinstance(expr, Identifier): 
     return ctx[expr.name] if expr.name in ctx else None
@@ -320,7 +357,7 @@ def exec_expr(expr, ctx={}):
   return expr
 
 def display_expr(expr):
-  '''Return a string representation of the given expression'''
+  '''Return a string representation of the given expression.'''
 
   if isinstance(expr, str): 
     disp = expr.replace("\\", "\\\\")
@@ -353,7 +390,7 @@ def display_expr(expr):
   return str(expr)
 
 def find_in_expr(expr, *idns):
-  '''Return true if any of the given identifiers or constant numbers are found in the given expression'''
+  '''Return true if any of the given identifiers or constant numbers are found in the given expression.'''
 
   if isinstance(expr, int): return expr in idns
   if isinstance(expr, Identifier): return expr.name in idns
@@ -364,12 +401,46 @@ def find_in_expr(expr, *idns):
 
   return False
 
-def execute(program, ctx=default_ctx, in_loop=False, in_func=False):
+def expr_equal(a, b):
+  '''Check if two expressions are semantically equivalent (!= equal after evaluation).'''
+
+  if a == b: return True
+  if not type(a) == type(b): return False
+
+  if isinstance(a, Identifier):
+    return a.name == b.name
+  if isinstance(a, FuncCall):
+    return a.name == b.name and all(expr_equal(aa,ba) for aa,ba in zip(a.args or [], b.args or []))
+
+  if isinstance(a, tuple):
+    if a[0] != b[0]: return False
+    if expr_equal(a[1], b[1]) and expr_equal(a[2], b[2]): return True
+    if expr_equal(a[1], b[2]) and expr_equal(a[2], b[1]): return a[0][0] in '+*=!'
+    return False
+
+  return False
+
+def simplify_expr(expr):
+  '''Simplify an expression if possible. Not very smart, so mostly constant folding.'''
+
+  if not isinstance(expr, tuple): return expr
+  if isinstance(expr, FuncCall): return expr
+  if isinstance(expr, Identifier): return expr
+
+  lho = simplify_expr(expr[1])
+  rho = simplify_expr(expr[2])
+
+  if isnumber(lho) and isnumber(rho):
+    return operators[expr[0]]({}, lho, rho)
+
+  return expr
+
+def execute(program, ctx=dict(default_ctx), in_loop=False, in_func=False):
   '''Execute a list of parsed statements'''
 
   for statement in program:
     # Sequence definitions
-    if isinstance(statement, Sequence):
+    if isinstance(statement, SequenceLike):
       ctx[statement.ident] = statement
       continue
 
@@ -434,6 +505,12 @@ def execute(program, ctx=default_ctx, in_loop=False, in_func=False):
     if isinstance(statement, ReturnStmt):
       if not in_func: raise Exception("Can't return outside of function")
       return exec_expr(statement.value, ctx)
+
+
+# Utils
+
+def isnumber(x):
+  return isinstance(x,int) or isinstance(x,float)
 
 
 # CLI
