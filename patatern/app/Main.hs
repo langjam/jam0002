d0eeccb68c52fe2e690e64b7bd7ab13f18d6da5f
Francesco Magliocca <franciman12@gmail.com>
2021-12-08 00:40:47 +0100

    Implement a patatern parser

--- patatern/app/Main.hs
+++ patatern/app/Main.hs
@@ -1,4 +1,12 @@
 module Main where
 
+import Parser
+import qualified Data.Text.IO as T
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    file <- T.readFile "example.pt"
+    case parseProgram "example.pt" file of
+        Left err -> putStrLn err
+        Right res -> print res
+
---
+++ patatern/app/Parser.hs
@@ -0,0 +1,81 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Parser where
+
+import Data.Text (Text)
+import Data.Char
+import qualified Data.Text as T
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+import SyntaxTree
+
+type Parser = Parsec Void Text
+
+sc :: Parser ()
+sc = L.space
+  space1
+  (L.skipLineComment ";")
+  (L.skipBlockComment "{-" "-}")
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+keyword :: Text -> Parser Text
+keyword = L.symbol sc
+
+isOperator :: Char -> Bool
+isOperator c = c `elem` ['+', '-', '*', '/', '?']
+
+symbol :: Parser Text
+symbol = T.pack <$> lexeme chars
+  where
+    chars = (:) <$> symbolStart <*> symbolCont
+    symbolStart = satisfy (\c -> isLower c || isOperator c)
+    symbolCont = many $ satisfy (\c -> isOperator c || isAlphaNum c)
+
+variable :: Parser Text
+variable = T.pack <$> lexeme chars
+  where
+    chars = (:) <$> symbolStart <*> symbolCont
+    symbolStart = upperChar
+    symbolCont = many alphaNumChar
+
+int :: Parser Integer
+int = lexeme L.decimal
+
+lazyPattern :: Parser (Term Text)
+lazyPattern = do
+    keyword "~"
+    Lazy <$> atom
+
+atom :: Parser (Term Text)
+atom = choice
+  [ between (keyword "(") (keyword ")") term
+  , Symbol <$> symbol
+  , Int <$> int
+  , Var <$> variable
+  , lazyPattern
+  ]
+
+term :: Parser (Term Text)
+term = do
+    ts <- some atom
+    return $ foldr1 (:<) ts
+
+rule :: Parser (Rule Text)
+rule = do
+    lhs <- term
+    keyword ":"
+    rhs <- sepBy term (keyword ",")
+    keyword "."
+    return $ Rule lhs rhs
+
+program :: Parser [Rule Text]
+program = many rule <* eof
+
+parseProgram :: String -> Text -> Either String [Rule Text]
+parseProgram filename input =
+  case parse (space *> program) filename input of
+    Left err -> Left $ errorBundlePretty err
+    Right res -> Right res
