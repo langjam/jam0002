66130bfdeb9b2d06823063be7274aabb6111a6f5
Katrina Scialdone <kat@swordglowsblue.com>
2021-12-06 22:15:38 -0700

    Dec 6: 3h, sequence arithmetic, operator associativity, impl return, refactoring

--- SeekWhence/src/main.py
+++ SeekWhence/src/main.py
@@ -5,15 +5,25 @@ import sys
 import math
 
 from parsy import *
-from dataclasses import dataclass
-from typing import Any
+from dataclasses import dataclass, replace as copy_dc
+from collections import namedtuple
 from codecs import escape_decode
 from time import time as systime
+from abc import ABC, abstractmethod
 
 # Datatypes
 
+class SequenceLike(ABC):
+  @abstractmethod
+  def generator(self, ctx={}):
+    pass
+
+  @abstractmethod
+  def value_at(self, n, ctx={}, prev=None):
+    pass
+
 @dataclass
-class Sequence:
+class Sequence(SequenceLike):
   exprs: tuple
   ident: str = "<anonymous>"
   baseCase: list = None
@@ -50,90 +60,62 @@ class Sequence:
   def __str__(self):
     exprs = ", ".join([display_expr(e) for e in self.exprs])
     if self.baseCase is not None:
-      basec = ", ".join([str(i) for i in self.baseCase])
+      basec = ", ".join([display_expr(i) for i in self.baseCase])
       return f"[sequence {self.ident}: {basec} | {exprs}]"
     return f"[sequence {self.ident}: {exprs}]"
 
+  def wrap_op(self, op, opr, flip=False, mode='both'):
+    if isinstance(opr, SequenceLike):
+      raise Exception(f'Arithmetic operations on multiple sequences are not currently supported')
+    else:
+      new_exprs = [(op, opr if flip else e, e if flip else opr) for e in self.exprs]
+      new_basec = [(op, opr if flip else e, e if flip else opr) for e in self.baseCase] if self.baseCase is not None else None
+
+      return copy_dc(self,
+        ident = f'<{display_expr(opr)}{op}{self.ident}>' if flip else f'<{self.ident}{op}{display_expr(opr)}>',
+        exprs = new_exprs if mode != 'base' else self.exprs,
+        baseCase = new_basec if mode != 'expr' else self.baseCase)
+
+  def __add__(self, other): return self.wrap_op('+', other)
+  def __sub__(self, other): return self.wrap_op('-', other)
+  def __mul__(self, other): return self.wrap_op('*', other)
+  def __truediv__(self, other): return self.wrap_op('/', other)
+  def __mod__(self, other): return self.wrap_op('%', other)
+  def __pow__(self, other): return self.wrap_op('^', other)
+
+  def __radd__(self, other): return self.wrap_op('+', other, True)
+  def __rsub__(self, other): return self.wrap_op('-', other, True)
+  def __rmul__(self, other): return self.wrap_op('*', other, True)
+  def __rtruediv__(self, other): return self.wrap_op('/', other, True)
+  def __rmod__(self, other): return self.wrap_op('%', other, True)
+  def __rpow__(self, other): return self.wrap_op('^', other, True)
+
 @dataclass
-class SequenceSlice:
-  ident: str
+class SequenceSlice(SequenceLike):
+  sequence: Sequence
   sliceIndx: int
 
   def generator(self, ctx={}):
-    if self.ident not in ctx or not isinstance(ctx[self.ident], Sequence):
-      raise Exception(f"Can't slice undefined sequence {self.ident}")
-
-    sequence = ctx[self.ident]
     current, prev = max(self.sliceIndx, 0), 0
     while True: 
-      yield (nextval := sequence.value_at(current, ctx, prev))
+      yield (nextval := self.sequence.value_at(current, ctx, prev))
       current, prev = current+1, nextval
 
   def value_at(self, n, ctx={}, prev=None):
-    if self.ident not in ctx or not isinstance(ctx[self.ident], Sequence):
-      raise Exception(f"Can't slice undefined sequence {self.ident}")
-
-    return ctx[self.ident].value_at(n+self.sliceIndx, ctx)
+    return self.sequence.value_at(n+self.sliceIndx, ctx)
 
   def __str__(self):
-    return f"[slice of {self.ident} from {self.sliceIndx}]"
+    return f"[slice {self.sequence.ident}::{self.sliceIndx}]"
 
-
-@dataclass
-class Identifier:
-  name: str
-
-  def __str__(self):
-    return self.name
-
-@dataclass
-class ForLoop:
-  amount: Any
-  srcseq: Any
-  ident: str
-  body: Any
-
-@dataclass
-class IfStmt:
-  cond: Any
-  body: Any
-  elseBlock: Any = None
-
-@dataclass
-class FuncDef:
-  ident: str
-  argNames: list
-  body: Any
-
-@dataclass
-class FuncCall:
-  name: str
-  args: list
-
-  def __str__(self):
-    args = ", ".join([display_expr(e) for e in self.args])
-    return f"[function {self.name}: {args}]"
-
-@dataclass
-class VarDef:
-  ident: str
-  expr: str
-
-@dataclass
-class SequenceAccess:
-  ident: str
-  index: Any
-
-  def __str__(self):
-    index = display_expr(self.index)
-    return f"{self.ident}:{index}"
-
-@dataclass
-class ReturnStmt:
-  value: Any
-
-BreakStmt    = {'__unique':"break"}
-InfiniteLoop = {'__unique':':inf'}
+Identifier   = namedtuple('ident',   ['name'])
+LoopStmt     = namedtuple('loop',    ['amount', 'srcseq', 'ident', 'body'])
+CondStmt     = namedtuple('cond',    ['cond', 'body', 'elseBlock'])
+FuncDefn     = namedtuple('func',    ['ident', 'argNames', 'body'])
+FuncCall     = namedtuple('call',    ['name', 'args'])
+VarDef       = namedtuple('setv',    ['ident', 'expr'])
+ReturnStmt   = namedtuple('_return', ['value'])
+BreakStmt    = namedtuple('_break',  [])()
+InfiniteLoop = namedtuple('_inflp',  [])()
 
 
 # Parser
@@ -165,25 +147,32 @@ def create_parser():
   string_lit        = (string_lit_double | string_lit_single).map(lambda s: escape_decode(bytes(s[1:-1], 'utf-8'))[0].decode('utf-8'))
 
   # Expressions
-  collapse_expr    = lambda l,o=None: l if o == None else (o[0], collapse_expr(l), collapse_expr(o[1]))
   expression       = forward_declaration()
   expressionInline = string('(') >> padding >> expression.optional() << padding << string(')')
   funcCall         = forward_declaration()
   funcCallInline   = string('[') >> padding >> funcCall << padding << string(']')
-  sequenceSlice    = seq(validident << string('::'), integer | identifier | funcCallInline | expressionInline).combine(SequenceSlice)
-  sequenceAccess   = seq(validident << string(':'), integer | identifier | funcCallInline | expressionInline).combine(SequenceAccess)
-  operand          = sequenceAccess | sequenceSlice | identifier | integer | string_lit | funcCallInline | expressionInline
+  operand          = identifier | integer | string_lit | funcCallInline | expressionInline
 
-  mulOp = seq(padding >> char_from('*/%') << padding, operand)
-  mulTm = seq(operand, mulOp.optional()).combine(collapse_expr)
+  def collapse_expr(lho, opr=None):
+    if opr is None: return lho
+    op, rho = opr[0]
+    expr = (op, collapse_expr(lho), collapse_expr(rho))
+    return collapse_expr(expr, opr[1:]) if len(opr) > 1 else expr
 
-  addOp = seq(padding >> char_from('+-') << padding, mulTm)
-  addTm = seq(mulTm, addOp.optional()).combine(collapse_expr)
+  opr_terms = [operand]
+  def define_opr(parse_op, suffixable=False):
+    nonlocal opr_terms
+    if suffixable: parse_op = seq(parse_op, char_from(':~').optional()).map(lambda x: x[0]+x[1] if x[1] else x[0])
+    op_right = seq(padding >> parse_op << padding, opr_terms[-1]).at_least(1)
+    opr_terms.append(seq(opr_terms[-1], op_right.optional()).combine(collapse_expr))
 
-  cmpOp = seq(padding >> string_from('<=', '<', '>=', '>', '==', '!=') << padding, addTm)
-  cmpTm = seq(addTm, cmpOp.optional()).combine(collapse_expr)
+  define_opr(string_from('::',':'))
+  define_opr(char_from('^'), True)
+  define_opr(char_from('*/%'), True)
+  define_opr(char_from('+-'), True)
+  define_opr(string_from('<=', '<', '>=', '>', '==', '!='))
 
-  expression.become(cmpTm)
+  expression.become(opr_terms[-1])
 
   # Code blocks and statements
   block      = forward_declaration()
@@ -214,7 +203,7 @@ def create_parser():
     ident    = validident << whitespace,
     argNames = (string('with') >> whitespace >> validident.sep_by(padding >> string(',') << padding, min=1) << whitespace).optional(),
     body     = block
-  ).combine_dict(FuncDef)
+  ).combine_dict(FuncDefn)
 
   funcCall.become(seq(
     name = validident,
@@ -230,7 +219,7 @@ def create_parser():
   ).combine_dict(VarDef)
 
   # For loops
-  forLoop = seq(
+  loopStmt = seq(
     _1     = string('for') << whitespace,
     amount = (expression | string(':inf').result(InfiniteLoop)) << whitespace,
     _2     = string('of') << whitespace,
@@ -239,7 +228,7 @@ def create_parser():
     ident  = validident << whitespace,
     _4     = string('do') << whitespace,
     body   = block
-  ).combine_dict(ForLoop)
+  ).combine_dict(LoopStmt)
 
   # Break and return
   breakStmt = string('break').result(BreakStmt)
@@ -250,16 +239,16 @@ def create_parser():
   ).combine_dict(ReturnStmt)
 
   # If/else statements
-  ifStmt = seq(
+  condStmt = seq(
     _1        = string('if') << whitespace,
     cond      = expression << whitespace,
     _2        = string('then') << whitespace,
     body      = block,
     elseBlock = (whitespace >> string('else') >> whitespace >> block).optional()
-  ).combine_dict(IfStmt)
+  ).combine_dict(CondStmt)
 
   block.become((string('{') >> statements << string('}')) | statement.map(lambda s: [s]))
-  statement.become(sequenceDef | forLoop | breakStmt | ifStmt | varDef | funcDef | funcCall)
+  statement.become(sequenceDef | loopStmt | breakStmt | returnStmt | condStmt | varDef | funcDef | funcCall)
 
   return statements
 
@@ -269,7 +258,7 @@ def create_parser():
 default_ctx = {
   'print': lambda _,*s: print(*s),
   'input': lambda _,p=None: (input() if p is None else input(p)) if sys.stdin.isatty() else sys.stdin.readline().rstrip('\n'),
-  'systime': lambda _: systime()*1000,
+  'systime': lambda _: math.floor(systime()*1000),
   'floor': lambda _,x: math.floor(x),
   'ceil': lambda _,x: math.ceil(x),
   'round': lambda _,x: math.round(x),
@@ -279,18 +268,36 @@ default_ctx = {
 }
 
 operators = {
-  '+': lambda x,y: x+y,
-  '-': lambda x,y: x-y,
-  '*': lambda x,y: x*y,
-  '/': lambda x,y: x/y if y != 0 else None,
-  '%': lambda x,y: x%y if y != 0 else None,
+  '+': lambda _,x,y: x+y,
+  '-': lambda _,x,y: x-y,
+  '*': lambda _,x,y: x*y,
+  '/': lambda _,x,y: x/y if y != 0 else None,
+  '%': lambda _,x,y: x%y if y != 0 else None,
+  '^': lambda _,x,y: x**y,
 
-  '<':  lambda x,y: x<y,
-  '<=': lambda x,y: x<=y,
-  '>':  lambda x,y: x>y,
-  '>=': lambda x,y: x>=y,
-  '==': lambda x,y: x==y,
-  '!=': lambda x,y: x!=y,
+  '+~': lambda _,x,y: x.wrap_op('+', y, flip=isinstance(y,Sequence), mode="base") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['+'](x,y),
+  '-~': lambda _,x,y: x.wrap_op('-', y, flip=isinstance(y,Sequence), mode="base") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['-'](x,y),
+  '*~': lambda _,x,y: x.wrap_op('*', y, flip=isinstance(y,Sequence), mode="base") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['*'](x,y),
+  '/~': lambda _,x,y: x.wrap_op('/', y, flip=isinstance(y,Sequence), mode="base") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['/'](x,y),
+  '%~': lambda _,x,y: x.wrap_op('%', y, flip=isinstance(y,Sequence), mode="base") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['%'](x,y),
+  '^~': lambda _,x,y: x.wrap_op('^', y, flip=isinstance(y,Sequence), mode="base") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['^'](x,y),
+
+  '+:': lambda _,x,y: x.wrap_op('+', y, flip=isinstance(y,Sequence), mode="expr") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['+'](x,y),
+  '-:': lambda _,x,y: x.wrap_op('-', y, flip=isinstance(y,Sequence), mode="expr") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['-'](x,y),
+  '*:': lambda _,x,y: x.wrap_op('*', y, flip=isinstance(y,Sequence), mode="expr") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['*'](x,y),
+  '/:': lambda _,x,y: x.wrap_op('/', y, flip=isinstance(y,Sequence), mode="expr") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['/'](x,y),
+  '%:': lambda _,x,y: x.wrap_op('%', y, flip=isinstance(y,Sequence), mode="expr") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['%'](x,y),
+  '^:': lambda _,x,y: x.wrap_op('^', y, flip=isinstance(y,Sequence), mode="expr") if isinstance(x,Sequence) or isinstance(y,Sequence) else operators['^'](x,y),
+
+  '<':  lambda _,x,y: x<y,
+  '<=': lambda _,x,y: x<=y,
+  '>':  lambda _,x,y: x>y,
+  '>=': lambda _,x,y: x>=y,
+  '==': lambda _,x,y: x==y,
+  '!=': lambda _,x,y: x!=y,
+
+  ':': lambda c,x,y: x.value_at(math.floor(y),c) if isinstance(x,SequenceLike) and (isinstance(y,int) or isinstance(y,float)) else None,
+  '::': lambda _,x,y: SequenceSlice(x,y) if isinstance(x,SequenceLike) and (isinstance(y,int) or isinstance(y,float)) else None
 }
 
 def exec_expr(expr, ctx={}):
@@ -299,15 +306,6 @@ def exec_expr(expr, ctx={}):
   if isinstance(expr, Identifier): 
     return ctx[expr.name] if expr.name in ctx else None
 
-  if isinstance(expr, SequenceAccess):
-    if expr.ident not in ctx or (not isinstance(ctx[expr.ident], Sequence) and not isinstance(ctx[expr.ident], SequenceSlice)):
-      raise Exception(f"Can't access undefined sequence {expr.ident}")
-    index = exec_expr(expr.index, ctx)
-    if isinstance(index, float): index = int(index)
-    if not isinstance(index, int):
-      raise Exception(f"Can't access sequence {expr.ident} by non-integer value")
-    return ctx[expr.ident].value_at(index, ctx)
-
   if isinstance(expr, FuncCall):
     if expr.name not in ctx or not callable(ctx[expr.name]):
       raise Exception(f"Can't call undefined function {expr.name}")
@@ -315,7 +313,7 @@ def exec_expr(expr, ctx={}):
 
   if isinstance(expr, tuple):
     if expr[0] in operators:
-      return operators[expr[0]](exec_expr(expr[1], ctx), exec_expr(expr[2], ctx))
+      return operators[expr[0]](ctx, exec_expr(expr[1], ctx), exec_expr(expr[2], ctx))
     raise Exception(f"Unknown operator {expr[0]}")
 
   return expr
@@ -324,10 +322,29 @@ def display_expr(expr):
   '''Return a string representation of the given expression'''
 
   if isinstance(expr, str): 
-    disp = expr.replace("\\", "\\\\").replace("\"", "\\\"")
+    disp = expr.replace("\\", "\\\\")
+    disp = expr.replace("\"", "\\\"")
+    disp = expr.replace("\n", "\\\n")
+    disp = expr.replace("\r", "\\\r")
+    disp = expr.replace("\t", "\\\t")
+    disp = expr.replace("\b", "\\\b")
+    disp = expr.replace("\f", "\\\f")
     return f'"{disp}"'
 
+  if isinstance(expr, Identifier):
+    return expr.name
+
+  if isinstance(expr, FuncCall):
+    if expr.args is not None:
+      args = ", ".join([display_expr(e) for e in expr.args])
+      return f"[call_function {expr.name}: {args}]"
+    return f"[call_function {expr.name}]"
+
   if isinstance(expr, tuple):
+    if expr[0] == ':':
+      return f"{display_expr(expr[1])}:{display_expr(expr[2])}"
+    if expr[0] == '::':
+      return f"{display_expr(expr[1])}::{display_expr(expr[2])}"
     if expr[0] in operators:
       return f"({display_expr(expr[1])} {expr[0]} {display_expr(expr[2])})"
     raise Exception("Unexpected tuple when displaying expression")
@@ -346,7 +363,7 @@ def find_in_expr(expr, *idns):
 
   return False
 
-def execute(program, ctx=default_ctx, in_loop=False):
+def execute(program, ctx=default_ctx, in_loop=False, in_func=False):
   '''Execute a list of parsed statements'''
 
   for statement in program:
@@ -356,25 +373,25 @@ def execute(program, ctx=default_ctx, in_loop=False):
       continue
 
     # If/else statements
-    if isinstance(statement, IfStmt):
+    if isinstance(statement, CondStmt):
       cond = exec_expr(statement.cond, ctx)
       if cond:
-        value = execute(statement.body, ctx, in_loop)
+        value = execute(statement.body, ctx, in_loop, in_func)
         if value is BreakStmt: return BreakStmt
       elif statement.elseBlock is not None:
-        value = execute(statement.elseBlock, ctx, in_loop)
+        value = execute(statement.elseBlock, ctx, in_loop, in_func)
         if value is BreakStmt: return BreakStmt
 
     # For loops
-    if isinstance(statement, ForLoop):
+    if isinstance(statement, LoopStmt):
       srcseq = exec_expr(statement.srcseq, ctx)
 
-      if not srcseq or (not isinstance(srcseq, Sequence) and not isinstance(srcseq, SequenceSlice)):
+      if not srcseq or not isinstance(srcseq, SequenceLike):
         raise Exception(f"Can't iterate over undefined sequence {statement.srcseq}")
 
       if statement.amount is InfiniteLoop:
         for value in srcseq.generator(ctx):
-          value = execute(statement.body, { **ctx, statement.ident: value }, True)
+          value = execute(statement.body, { **ctx, statement.ident: value }, True, in_func)
           if value is BreakStmt: break
       else:
         maxiters, numiters = exec_expr(statement.amount, ctx), 0
@@ -382,7 +399,7 @@ def execute(program, ctx=default_ctx, in_loop=False):
           raise Exception(f"Amount expression of for loop must evaluate to an integer")
 
         for value in srcseq.generator(ctx):
-          value = execute(statement.body, { **ctx, statement.ident: value }, True)
+          value = execute(statement.body, { **ctx, statement.ident: value }, True, in_func)
           if value is BreakStmt: break
           numiters += 1
           if numiters >= maxiters: break
@@ -390,12 +407,12 @@ def execute(program, ctx=default_ctx, in_loop=False):
       continue
 
     # Function definitions and calls
-    if isinstance(statement, FuncDef):
+    if isinstance(statement, FuncDefn):
       def create_user_func(fdef):
         def exec_user_func(ctx, *args):
           arity = len(fdef.argNames) if fdef.argNames is not None else 0
           if len(args) != arity: raise Exception(f"Function {fdef.ident} expects {arity} arguments, got {len(args)}")
-          return execute(fdef.body, { **ctx, **{ fdef.argNames[i]: v for i,v in enumerate(args) } })
+          return execute(fdef.body, { **ctx, **{ fdef.argNames[i]: v for i,v in enumerate(args) } }, False, True)
         return exec_user_func
       ctx[statement.ident] = create_user_func(statement)
 
@@ -411,6 +428,11 @@ def execute(program, ctx=default_ctx, in_loop=False):
     if statement is BreakStmt:
       if not in_loop: raise Exception("Can't break outside of loop")
       return BreakStmt
+
+    # Return statements
+    if isinstance(statement, ReturnStmt):
+      if not in_func: raise Exception("Can't return outside of function")
+      return exec_expr(statement.value, ctx)
 
 
 # CLI
