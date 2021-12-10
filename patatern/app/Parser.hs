fa52de03023be5579b8f6f6036937af31b3b1137
Francesco Magliocca <franciman12@gmail.com>
2021-12-10 10:05:42 +0100

    Parse signed integers

--- patatern/app/Parser.hs
+++ patatern/app/Parser.hs
@@ -25,7 +25,7 @@ keyword :: Text -> Parser Text
 keyword = L.symbol sc
 
 isOperator :: Char -> Bool
-isOperator c = c `elem` ['+', '-', '*', '/', '?', '=']
+isOperator c = c `elem` ['+', '-', '*', '/', '?', '=', '<', '>']
 
 symbol :: Parser Text
 symbol = T.pack <$> lexeme chars
@@ -42,7 +42,8 @@ variable = T.pack <$> lexeme chars
     symbolCont = many $ satisfy (\c -> isAlphaNum c || isOperator c)
 
 int :: Parser Integer
-int = lexeme L.decimal
+int = lexeme (L.signed (return ()) L.decimal <* notFollowedBy nonDigit)
+  where nonDigit = satisfy (\c -> isOperator c || isAlpha c)
 
 list :: Parser (Term Text)
 list = do
@@ -53,8 +54,10 @@ atom :: Parser (Term Text)
 atom = choice
   [ between (keyword "(") (keyword ")") term
   , between (keyword "[") (keyword "]") list
+  -- We parse plus or minus symbols on their own
+  -- to avoid ambiguity with signs of numbers
+  , Int <$> try int
   , Symbol <$> symbol
-  , Int <$> int
   , Var <$> variable
   ]
 
