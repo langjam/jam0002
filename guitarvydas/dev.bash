98d8ff0f341e88648f3105f8b3c408e1c27a530b
Paul Tarvydas <guitarvydas@gmail.com>
2021-12-10 12:25:48 -0500

    os.c compiles

--- guitarvydas/dev.bash
+++ guitarvydas/dev.bash
@@ -9,24 +9,31 @@ pfrdir=${lj2}/pfrsrc
 d2fdir=${lj2}/d2fsrc
 f2jdir=${lj2}/f2jsrc
 
+# echo transiled files
+# echo producer.cmm '->' producer.c
+# ${pfr} ${lj2} producer.cmm cmm.ohm cmm.glue ${lj2}/support.js >producer.c
+
+# echo gcc producer.c
+# gcc producer.c
+
+# echo transiled files
+# echo consumer.cmm '->' consumer.c
+# ${pfr} ${lj2} consumer.cmm cmm.ohm cmm.glue ${lj2}/support.js >consumer.c
+
+# echo gcc consumer.c
+# gcc consumer.c
+
+
+# echo transiled files
+# echo main.cmm '->' main.c
+# ${pfr} ${lj2} main.cmm cmm.ohm cmm.glue ${lj2}/support.js >main.c
+
+# echo gcc main.c
+# gcc main.c
+
 echo transiled files
-echo producer.cmm '->' producer.c
-${pfr} ${lj2} producer.cmm cmm.ohm cmm.glue ${lj2}/support.js >producer.c
+echo os.cmm '->' os.c
+${pfr} ${lj2} os.cmm cmm.ohm cmm.glue ${lj2}/support.js >os.c
 
-echo gcc producer.c
-gcc producer.c
-
-echo transiled files
-echo consumer.cmm '->' consumer.c
-${pfr} ${lj2} consumer.cmm cmm.ohm cmm.glue ${lj2}/support.js >consumer.c
-
-echo gcc consumer.c
-gcc consumer.c
-
-
-echo transiled files
-echo main.cmm '->' main.c
-${pfr} ${lj2} main.cmm cmm.ohm cmm.glue ${lj2}/support.js >main.c
-
-echo gcc main.c
-gcc main.c
+echo gcc os.c
+gcc os.c
